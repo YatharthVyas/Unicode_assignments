2fe79f406e770907dd1c400ea8f48b9269c6bc57
YatharthVyas <yatharth2000.vyas@gmail.com>
2019-10-06 09:49:42 +0530

    Added more CSS styling

--- Task 2/form/src/Components/form.js
+++ Task 2/form/src/Components/form.js
@@ -4,23 +4,23 @@ class Form extends Component{
 	render(){
 		if ((this.props.firstName + this.props.lastName)!=="" && this.props.age !=="")
 		return(
-        <div>
-        	Your Name is {this.props.firstName + " " + this.props.lastName}<br/>
-        	Your Age is {this.props.age}<br/>
-        </div>
-    	)
+	        <div>
+	        	Your Name is {this.props.firstName + " " + this.props.lastName}<br/>
+	        	Your Age is {this.props.age}<br/>
+	        </div>
+	    	)
 		else if ((this.props.firstName + this.props.lastName)!=="")
-		return(
-        <div>
-        	Your Name is {this.props.firstName + " " + this.props.lastName}<br/>
-        </div>
-        )
+			return(
+	        <div>
+	        	Your Name is {this.props.firstName + " " + this.props.lastName}<br/>
+	        </div>
+	        )
 		else if (this.props.age !=="")
-		return(
-        <div>	
-			Your Age is {this.props.age}<br/>
-        </div>
-        )
+			return(
+	        <div>	
+				Your Age is {this.props.age}<br/>
+	        </div>
+	        )
 		else
 			return(
 				<div>
